539d2f3862b1b1e32558ac43fdeff2e44b580482
Tran Xuan Hoang <hoangtx.ict@gmail.com>
2017-06-04 15:32:28 +0900

    Add class Down.java

---
+++ src/layer1/Down.java
@@ -0,0 +1,27 @@
+/**
+ * Component type: Coordinator
+ * Purpose: Put down the robot's hand near the surface of the
+ * table to put down an item on its hand, or to prepare to
+ * pick an item on the table.
+ * 
+ * @author Tran Xuan Hoang
+ */
+package layer1;
+
+import layer1.RobotHand.Status;
+
+public class Down implements Operation {
+	@Override
+	public void execute(TableCell[][] table, RobotHand rbh) {
+		rbh.posStatus = Status.DOWN;
+	} // end method PutDown
+
+	public String toString() {
+		return "DN";
+	} // end method toString
+
+	@Override
+	public String getOpName() {
+		return "Down";
+	}
+} // end class Down
